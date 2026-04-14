fb5c8fc4b291d381061c2501869e8ebb0db831a5
MMartin <94718213+SupplyITA@users.noreply.github.com>
2026-04-14 11:14:21 +0200

    Correzione

--- progetto/backend/src/parser_wikipedia.py
+++ progetto/backend/src/parser_wikipedia.py
@@ -1,26 +1,4 @@
 import asyncio
-<<<<<<< HEAD
-from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
-
-async def main():
-    # configura il browser (headless = senza finestra visibile)
-    browser_cfg = BrowserConfig(headless=False)
-
-    # configura la richiesta (BYPASS = scarica sempre dalla rete, ignora cache)
-    crawler_cfg = CrawlerRunConfig(cache_mode=CacheMode.BYPASS) '''css_selector="#bodyContent")'''
-
-    # apre il browser e lo chiude automaticamente alla fine del blocco
-    async with AsyncWebCrawler(config=browser_cfg) as crawler:
-
-        # visita la pagina e aspetta che il crawl sia completato
-        result = await crawler.arun(
-            url="https://en.wikipedia.org/wiki/Horse",
-            config=crawler_cfg
-        )
-
-        
-
-=======
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
@@ -103,18 +81,11 @@ async def parse_wikipedia(url: str) -> dict:
             "parsed_text": parsed_text
         }
         
->>>>>>> origin/pippo
         # result.success        → True se il crawl è andato a buon fine
         # result.error_message  → messaggio d'errore in caso di fallimento
         # result.markdown       → testo in Markdown, pronto per LLM e RAG
         # result.cleaned_html   → HTML ripulito da script, stili e rumore
         # result.html           → HTML completo della pagina (non ripulito)
-<<<<<<< HEAD
-        print(result.markdown)
-
-# avvia il programma asincrono
-asyncio.run(main())
-=======
 
 # test
 if __name__ == "__main__":
@@ -132,5 +103,3 @@ if __name__ == "__main__":
         print("Parsing completato con successo!")
     except Exception as e:
         print(f"Si è verificato un errore: {e}")
-
->>>>>>> origin/pippo
